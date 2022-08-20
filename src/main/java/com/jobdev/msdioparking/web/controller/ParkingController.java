83196beb86251b9847dc325c690911921a7ee997
jobdev-hub <job.methods@outlook.com>
2022-08-20 13:58:42 -0300

    [ResponseEntity] exemplo padrão de retorno api

--- src/main/java/com/jobdev/msdioparking/web/controller/ParkingController.java
+++ src/main/java/com/jobdev/msdioparking/web/controller/ParkingController.java
@@ -1,10 +1,10 @@
 package com.jobdev.msdioparking.web.controller;
 
 import com.jobdev.msdioparking.domain.dto.ParkingDTO;
-import com.jobdev.msdioparking.domain.entity.Parking;
 import com.jobdev.msdioparking.service.ParkingService;
 import com.jobdev.msdioparking.web.mapper.ParkingMapper;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +24,9 @@ public class ParkingController {
 
     @GetMapping
     @Operation(summary = "Get all parkings", description = "return list of parkings")
-    public List<ParkingDTO> findAll() {
-        List<Parking> parkingList = parkingService.findAll();
-        return parkingMapper.toParkingDTOList(parkingList);
+    public ResponseEntity<List<ParkingDTO>> findAll() {
+        var parkingList = parkingService.findAll();
+        var parkingDTOList = parkingMapper.toParkingDTOList(parkingList);
+        return ResponseEntity.ok(parkingDTOList);
     }
 }
