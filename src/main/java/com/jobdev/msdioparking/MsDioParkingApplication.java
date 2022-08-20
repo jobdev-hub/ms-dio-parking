288966cafc08386c741b5fcd7e9ba2cff8b57423
jobdev-hub <job.methods@outlook.com>
2022-08-20 13:46:36 -0300

    adiciona swagger

--- src/main/java/com/jobdev/msdioparking/MsDioParkingApplication.java
+++ src/main/java/com/jobdev/msdioparking/MsDioParkingApplication.java
@@ -1,9 +1,12 @@
 package com.jobdev.msdioparking;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ms-parking", version = "1.0.0"))
 public class MsDioParkingApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/jobdev/msdioparking/web/controller/HelloController.java
+++ src/main/java/com/jobdev/msdioparking/web/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.jobdev.msdioparking.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
+    @Operation(hidden = true)
     public String hello() {
         return "Hello Controller";
     }
